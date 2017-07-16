ca376222fd89d3a827b1acac2ff2ff419fa3a422
Sheshbabu <sheshbabu@redmart.com>
2017-07-16 16:45:48 +0800

    Using wpt-ui dep

---
+++ index.js
@@ -0,0 +1,10 @@
+const wptui = require("wpt-ui");
+
+wptui.init({
+  url: process.env.WEBPAGETEST_URL,
+  k: process.env.WEBPAGETEST_KEY,
+  location: "ec2-ap-southeast-1:Chrome.Cable",
+  runs: 5
+});
+
+wptui.start();
