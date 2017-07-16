33a06e77b9c0e800ff3915207e5b1d75df1b5a71
Sheshbabu <sheshbabu@redmart.com>
2017-07-16 20:19:29 +0800

    Setting port to 80

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ wptui.init({
   url: process.env.WEBPAGETEST_URL,
   k: process.env.WEBPAGETEST_KEY,
   location: "ec2-ap-southeast-1:Chrome.Cable",
-  runs: 5
+  runs: 5,
+  port: 80
 });
 
 wptui.start();
