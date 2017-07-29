ad7f8230cb050660e63240bf91b3e547a6719dee
Sheshbabu <sheshbabu@redmart.com>
2017-07-29 12:58:19 +0800

    Updated to new interface; Passing db connection string

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const wptui = require("wpt-ui");
 
-wptui.init({
+wptui.start({
   wptUrl: process.env.WEBPAGETEST_URL,
   wptApiKey: process.env.WEBPAGETEST_KEY,
   wptLocation: "ec2-ap-southeast-1:Chrome.Cable",
   wptRuns: 5,
+  dbConnectionString: process.env.DATABASE_URL,
   port: process.env.PORT
 });
-
-wptui.start();
