840fec8b574476ce5ef0442e8e74c953ab548f0c
Sheshbabu <sheshbabu@redmart.com>
2017-08-15 20:44:29 +0800

    Replaced wptPingbackUrl with publicUrl

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ wptui.start({
   wptUrl: process.env.WEBPAGETEST_URL,
   wptApiKey: process.env.WEBPAGETEST_KEY,
   wptLocation: "ec2-ap-southeast-1:Chrome.Cable",
-  wptPingbackUrl: process.env.WEBPAGETEST_PINGBACK_URL,
   wptRuns: 5,
+  publicUrl: process.env.PUBLIC_URL,
   dbConnectionString: process.env.DATABASE_URL,
   port: process.env.PORT
 });
